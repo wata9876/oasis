00339b73666160fe80d7b40437a6b30490ac66cb
wata <katsu.watanabe55@gmail.com>
2020-10-06 13:24:07 +0900

    DB名変更

--- src/oasisx/db/migrate.go
+++ src/oasisx/db/migrate.go
@@ -12,7 +12,7 @@ func main() {
 	USER := "root"
 	PASS := "1234"
 	PROTOCOL := "tcp(127.0.0.1:3306)"
-	DBNAME := "oasis"
+	DBNAME := "sample"
 
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
 	//db, err := gorm.Open(DBMS, CONNECT)
